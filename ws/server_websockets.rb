1c46618290bf0fc1b054f10792313a909a194446
Mikhail Eremeev <meremeev@sfedu.ru>
2022-04-09 12:19:25 +0300

    feat: add websockets server example

---
+++ ws/server_websockets.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+require 'em-websocket'
+
+EM.run do
+  @concurrentClientCount = 0
+  EM::WebSocket.run(:host => "0.0.0.0", :port => 28563) do |ws|
+    ws.onopen do |handshake|
+      @concurrentClientCount += 1
+      puts "#{@concurrentClientCount} concurrent clients are connected"
+    end
+
+    ws.onclose { @concurrentClientCount -= 1 }
+
+    ws.onmessage do |query|
+      puts "Data received: "+query
+      words = query.split(" ")
+      answer = (eval words[2]+words[1]+words[3]).to_s + "\r\n"
+      ws.send answer
+      puts "Answer: "+answer
+    end
+  end
+end
