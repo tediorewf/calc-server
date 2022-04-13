471c2da9a499467e201564ec7d0ae0481832c2b1
Mikhail Eremeev <meremeev@sfedu.ru>
2022-04-13 15:02:28 +0300

    refactor(ws): apply rubocop suggestions

--- ws/server_websockets.rb
+++ ws/server_websockets.rb
@@ -4,21 +4,22 @@
 require 'em-websocket'
 
 EM.run do
-  @concurrentClientCount = 0
-  EM::WebSocket.run(:host => "0.0.0.0", :port => 28563) do |ws|
+  @concurrent_client_count = 0
+  
+  EM::WebSocket.run(host: '0.0.0.0', port: 28_563) do |ws|
     ws.onopen do |handshake|
-      @concurrentClientCount += 1
-      puts "#{@concurrentClientCount} concurrent clients are connected"
+      @concurrent_client_count += 1
+      puts "#{@concurrent_client_count} concurrent clients are connected"
     end
 
-    ws.onclose { @concurrentClientCount -= 1 }
+    ws.onclose { @concurrent_client_count -= 1 }
 
     ws.onmessage do |query|
-      puts "Data received: "+query
+      puts "Data received: #{query}"
       words = query.split(" ")
-      answer = (eval words[2]+words[1]+words[3]).to_s + "\r\n"
+      answer = "#{(eval words[2]+words[1]+words[3]).to_s}\r\n"
       ws.send answer
-      puts "Answer: "+answer
+      puts "Answer: #{answer}"
     end
   end
 end
